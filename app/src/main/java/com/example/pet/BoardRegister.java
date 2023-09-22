aa318cc4ecf810403903fb9883516a22dbc0b41c
RohSowon <sowon4590@gachon.ac.kr>
2023-09-23 00:12:10 +0900

    파일 구성 수정 activity board fragment 삭제 board list item xml 파일 추가 activity board list 추가

--- app/src/main/java/com/example/pet/BoardRegister.java
+++ app/src/main/java/com/example/pet/BoardRegister.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class board_register extends AppCompatActivity {
+public class BoardRegister extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
